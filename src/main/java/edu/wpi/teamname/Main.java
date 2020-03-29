7bc5a8a491322b3b7042bdfee74ad3eaa5a0c067
Victoria Grasso <vygrasso@wpi.edu>
2020-03-29 19:08:49 -0400

    Added Victoria Grasso

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     App.launch(App.class, args);
+    System.out.println("Victoria Grasso");
     System.out.println("jimmy casella");
     System.out.println("Gregory Conrad");
     System.out.println("Collin Broderick");
